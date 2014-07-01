c397300251ff5adcb200b4db97320401d757ce34
Adam Maxwell <catalyst256@gmail.com>
2014-07-01 14:23:03 +0100

    Added a notes file to explain some of my madness in the initial coding

--- spidan.py
+++ spidan.py
@@ -37,7 +37,6 @@ def get_robots(target):
   else:
     print RED + 'No robots.txt found' + END
 
-
 if __name__ == '__main__':
   if len(sys.argv) != 2:
     print RED + 'Usage: ./spidan.py http://www.example.com' + END
